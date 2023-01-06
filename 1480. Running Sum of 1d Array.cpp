6b2b07ac62683cab9ef3e728a0597ded517769ae
Nguyen Xuan Phong <105656840+wind2715@users.noreply.github.com>
2023-01-06 15:32:28 +0700

    Create 1480. Running Sum of 1d Array.cpp

---
+++ 1480. Running Sum of 1d Array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        vector<int> sum(nums.size());
+        sum[0]=nums[0];
+        for(int i=1; i<nums.size(); i++)
+        {
+            sum[i]=sum[i-1]+nums[i];
+        }
+        return sum;
+    }
+};
